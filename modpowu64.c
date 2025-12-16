58e8ebfa66a41b870730ce3fc71e141de2c4a232
Simon Goater <124365713+FastAsChuff@users.noreply.github.com>
2025-12-16 10:40:45 +0000

    Add files via upload

--- modpowu64.c
+++ modpowu64.c
@@ -30,6 +30,14 @@ uint64_t fromu64mg(uint64_t ar, uint64_t n, uint64_t ninv, uint64_t twoto64modn)
   return (sumu64 + (sum < max128)*twoto64modn) % n;       
 }
 
+uint64_t modsumu64mg(uint64_t ar, uint64_t br, uint64_t n, uint64_t ninv, uint64_t twoto64modn) {
+  uint64_t sum = ar+br;
+  if (sum > ar) return sum;
+  sum += twoto64modn;
+  if (sum > twoto64modn) return sum;
+  return ((unsigned __int128)ar + br) % n;
+}
+
 uint64_t modprodu64mg(uint64_t ar, uint64_t br, uint64_t n, uint64_t ninv, uint64_t twoto64modn) {
   // n must be odd.
   // ar and br must be in Montgomery form with r = 2^64.
